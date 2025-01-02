790e7ee753caeb26d308db2534ed1e35cf2ca7d8
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-03 00:05:16 +0530

    changes saved

--- DSA(love babbar)/problems/LECTURE WISE/L-71(BINARY SEARCH TREE)[INORDER USE]/4-BST_from_preOrder_traversal.cpp
+++ DSA(love babbar)/problems/LECTURE WISE/L-71(BINARY SEARCH TREE)[INORDER USE]/4-BST_from_preOrder_traversal.cpp
@@ -1,6 +1,6 @@
 #include "../L-69(BINARY SEARCH TREE)/myclass2.h" 
 
-node* solve(vector<int> pre , int i , int mini , int maxi) // starting mini = int_min , maxi = int_max
+node* solve(vector<int> pre , int &i , int mini , int maxi) // starting mini = int_min , maxi = int_max
 {
     if (i>=pre.size() || pre[i]<mini || pre[i]>maxi)
     {
@@ -10,8 +10,8 @@ node* solve(vector<int> pre , int i , int mini , int maxi) // starting mini = in
     node* root = new node(pre[i]); // i iterate krke btata hai kis data ke correspond node  bnani hai 
     i++;
 
-    root->left = solve(pre , i , mini , pre[i]);
-    root->right = solve(pre , i , pre[i] , maxi);
+    root->left = solve(pre , i , mini , root->data);
+    root->right = solve(pre , i , root->data , maxi);
 
     return root ;
 }
