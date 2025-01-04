184f50214672a6b19dc38f5c9762ce50c833ffb6
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-04 12:17:24 +0530

    fixing shortest paths

--- DSA(love babbar)/problems/LECTURE WISE/L-93(GRAPHS)/shortest_path_unDirected_graph.cpp
+++ DSA(love babbar)/problems/LECTURE WISE/L-93(GRAPHS)/shortest_path_unDirected_graph.cpp
@@ -1,4 +1,4 @@
-// shortest distance based on length of path in undirected/directed graph (both applicable)
+// this gives the shortest path from startNode to endNode in an unDirected graph.
 
 #include<iostream>
 #include<list>
@@ -46,3 +46,34 @@ int shortestPath (unordered_map<int,list<int>> adj , unordered_map<int,bool> &vi
 }
 // why -> ex - ans is 8,3,1 (end = 8 , start = 1)
 // but ans should be 1,3,8 .
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// this gives array of shortest distance from start node to all other nodes in an unDirected graph.
+
+unordered_map<int, int> shortestPath(unordered_map<int, list<int>> adj, unordered_map<int, bool>& visited, int startNode) {
+    unordered_map<int, int> distance;
+    for (auto node : adj) {
+        distance[node.first] = INT_MAX; // Initialize all distances to infinity
+    }
+    distance[startNode] = 0; // Distance to startNode is 0
+
+    queue<int> q;
+    q.push(startNode);
+    visited[startNode] = true;
+
+    while (!q.empty()) {
+        int frontNode = q.front();
+        q.pop();
+
+        for (auto neighbor : adj[frontNode]) {
+            if (!visited[neighbor]) {
+                q.push(neighbor);
+                visited[neighbor] = true;
+                distance[neighbor] = distance[frontNode] + 1; // Update shortest distance
+            }
+        }
+    }
+
+    return distance;
+}
