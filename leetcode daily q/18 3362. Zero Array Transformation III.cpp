2c4945882738473b9a7db0283bfaf33974f17754
pihu26112005 <piyushkumar26november@gmail.com>
2025-05-25 18:43:29 +0530

    Add implementation for Zero Array Transformation III problem

---
+++ leetcode daily q/18 3362. Zero Array Transformation III.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+        int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+            sort(queries.begin(), queries.end());
+            priority_queue<int> available;
+            priority_queue<int, vector<int>, greater<int>> assigned;
+            int count = 0;
+    
+            for (int i = 0, k = 0; i < nums.size(); i++) {
+    
+                while (!assigned.empty() && assigned.top() < i)
+                    assigned.pop();
+    
+                while (k < queries.size() && queries[k][0] <= i) 
+                    available.push(queries[k++][1]);
+    
+                while (assigned.size() < nums[i] && 
+                      !available.empty() && available.top() >= i) {
+                    assigned.push(available.top());
+                    available.pop();
+                    count++;
+                }
+                
+                if (assigned.size() < nums[i])
+                    return -1;
+            }
+            return queries.size() - count;
+        }
+    };
+    
+    // Ist part q me sari queries ka use krke zero array banana tha 
+    // IInd me order me start se leke min kitni query ka use krke zero array ban sakti hai vo batana hai 
+    // IIIrd part me batana hai min kitni query ka use krke zero array bana sakte ho
