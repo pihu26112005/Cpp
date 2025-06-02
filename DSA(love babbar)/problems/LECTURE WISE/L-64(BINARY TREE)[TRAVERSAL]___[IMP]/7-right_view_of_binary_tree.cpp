3e02ff337f358934a9d252f040983b9abff740a1
pihu26112005 <piyushkumar26november@gmail.com>
2025-06-03 00:23:14 +0530

    better

--- DSA(love babbar)/problems/LECTURE WISE/L-64(BINARY TREE)[TRAVERSAL]___[IMP]/7-right_view_of_binary_tree.cpp
+++ DSA(love babbar)/problems/LECTURE WISE/L-64(BINARY TREE)[TRAVERSAL]___[IMP]/7-right_view_of_binary_tree.cpp
@@ -49,3 +49,39 @@ vector<int> getLeftView(node *root)
     levelOrderTraversal(q, ans);
     return ans ;
 }
+
+
+
+-----------
+
+
+
+class Solution {
+    public:
+        vector<int> rightSideView(TreeNode* root) {
+            vector<int> res;
+            queue<TreeNode*> q;
+            if (root) q.push(root);
+    
+            while (!q.empty()) {
+                TreeNode* rightSide = nullptr;
+                int size = q.size();
+    
+                for (int i = 0; i < size; ++i) {
+                    TreeNode* node = q.front(); q.pop();
+                    if (node) {
+                        rightSide = node;
+                        if (node->left) q.push(node->left);
+                        if (node->right) q.push(node->right);
+                    }
+                }
+    
+                if (rightSide) {
+                    res.push_back(rightSide->val);
+                }
+            }
+    
+            return res;
+            
+        }
+    };
